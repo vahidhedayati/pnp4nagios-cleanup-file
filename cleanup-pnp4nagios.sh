6be07a722a623422eb36c5aefd97fea773d8930c
akomakom <akomakom@users.noreply.github.com>
2015-12-10 13:54:07 -0500

    escaping parenthesi for egrep (didn't work as is)

--- cleanup-pnp4nagios.sh
+++ cleanup-pnp4nagios.sh
@@ -2,7 +2,7 @@
 
 folder="${1:-/var/pnp4nagios/perfdata}"
 
-files=$(egrep -l "(expected [0-9]* data source readings (got [0-9]*) from|found extra data on update argument)" $(find ${folder} -name '*.xml'))
+files=$(egrep -l "(expected [0-9]* data source readings \(got [0-9]*\) from|found extra data on update argument)" $(find ${folder} -name '*.xml'))
 
 for found in $(echo $files); do
   filename=${found%.*} 
