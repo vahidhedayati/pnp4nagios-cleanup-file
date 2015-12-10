3082dd4c0dcc869283a6dfbf10f7915d2d9cc2ca
akomakom <akomakom@users.noreply.github.com>
2015-12-10 13:36:44 -0500

    Significantly faster file search for large trees, overridable default directory

--- cleanup-pnp4nagios.sh
+++ cleanup-pnp4nagios.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-folder="/var/pnp4nagios/perfdata"
+folder="${1:-/var/pnp4nagios/perfdata}"
 
-#files=$(grep -r "expected [0-9]* data source readings (got [0-9]*) from" $folder/*|awk -F":" '{print $1}')
-files=$(egrep -r "(expected [0-9]* data source readings (got [0-9]*) from|found extra data on update argument)" $folder/*|awk -F":" '{print $1}')
+files=$(egrep -l "(expected [0-9]* data source readings (got [0-9]*) from|found extra data on update argument)" $(find ${folder} -name '*.xml'))
+
 for found in $(echo $files); do
   filename=${found%.*} 
 	echo "Removing $filename.xml -- $filename.rrd"
